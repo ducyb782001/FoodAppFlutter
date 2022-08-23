e5cf4e6f982569500c653bde4e89436bce161bad
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-08-23 10:14:14 +0700

    add counter app

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:counter_app/blocks/counter_block.dart';
+import 'package:counter_app/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Install bloc
+  // We have 2 events => Increase/ Decrease
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
